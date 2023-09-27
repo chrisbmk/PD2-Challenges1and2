f93b12007f8124d3e12723d49ab1fc69809635a2
Christopher Boamah-Mensah <ckz9@wildcats.unh.edu>
2023-09-27 00:25:29 -0400

    challenge1 is pretty much done

--- force-app/main/default/lwc/pokeCard/pokeCard.js
+++ force-app/main/default/lwc/pokeCard/pokeCard.js
@@ -1,13 +1,42 @@
 import { LightningElement } from 'lwc';
 
 export default class PokeCard extends LightningElement {
-  pokeName;
+  pokeName = null;
+  _pokeName = null;
+  spriteImgUrl;
+  abilities = [];
+  moves = [];
 
-  findPoke() {
-    console.log('findPoke');
+  handleChange(event) {
+    this.pokeName = event.detail.value;
   }
 
+  findPoke() {
+    this.spriteImgUrl = null;
+    this.abilities = [];
+    this.moves = [];
+    this._pokeName = null;
+
+    fetch('https://pokeapi.co/api/v2/pokemon/' + this.pokeName, { method: 'GET' })
+      .then((response) => response.json())
+      .then((data) => {
+        this._pokeName = data.name;
+        this.spriteImgUrl = data.sprites.front_default;
+
+        for (let i = 0; i < data.abilities.length; i++) {
+          this.abilities[i] = data.abilities[i].ability.name;
+        }
+        for (let j = 0; j < data.moves.length; j++) {
+          this.moves[j] = data.moves[j].move.name;
+        }
+      });
+    }
+
   reset() {
-    console.log('reset');
+    this.pokeName = null;
+    this._pokeName = null;
+    this.spriteImgUrl = null;
+    this.abilities = [];
+    this.moves = [];
   }
 }
